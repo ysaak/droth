df8e7bfe2667d94cff831cc69bc11c742a92526e
Damien ROTH <damien.roth@gmail.com>
2015-03-22 22:12:30 +0100

    New responsive layout + new header

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var gulp = require('gulp');
 var less   = require('gulp-less'),
     minifyCSS = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
+    gconcat = require('gulp-concat'),
     path   = require('path');
 
 var source = "assets/";
 var dist = "source/";
 
-gulp.task('less', function () {  
+gulp.task('less', function () {
     gulp.src(source + 'less/style.less')
         .pipe(less()) // Compile LESS
         .on('error', function(err){ console.log(err.message); })
@@ -18,12 +19,16 @@ gulp.task('less', function () {
         }))
         .on('error', function(err){ console.log(err.message); })
 //        .pipe(minifyCSS()) // Minify CSS
-        .pipe(gulp.dest(dist + 'css'));
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('scripts', function () {
+  gulp.src(source + 'js/*.js')
+      .pipe(gconcat('scripts.js'))
+      .pipe(gulp.dest(dist))
 });
 
 gulp.task('default', function() {
-
   gulp.watch(source + 'less/*.less', ['less']);
-
-    // place code for your default task here
+  gulp.watch(source + 'js/*.js', ['scripts']);
 });
